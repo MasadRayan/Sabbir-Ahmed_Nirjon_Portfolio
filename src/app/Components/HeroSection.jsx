593bfed1a5b1a197fdf8d11b4e27fa53bcb0a863
MasadRayan <masadrayan2002@gmail.com>
2025-12-12 14:15:56 +0600

    hero section modified

--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaGithubSquare } from "react-icons/fa";
-import { FaEnvelope } from "react-icons/fa";
+import { FaEnvelope, FaResearchgate, FaLinkedin } from "react-icons/fa";
 const HeroSection = () => {
     return (
         <section id="home" className="py-12 md:py-16 lg:py-20">
@@ -15,7 +15,7 @@ const HeroSection = () => {
                     >
                         {/* Profile Image */}
                         <div className="w-48 h-48 md:w-56 md:h-56 rounded-full bg-linear-to-br from-muted to-muted/50 r shadow-lg flex items-center justify-center overflow-hidden">
-                            <Image 
+                            <Image
                                 src="/profile.jpg"
                                 alt="Profile"
                                 width={200}
@@ -27,27 +27,30 @@ const HeroSection = () => {
                         {/* Name and Title */}
                         <div className="mt-6 space-y-1">
                             <h1 className="text-2xl md:text-3xl font-heading font-bold tracking-wide text-foreground">
-                                Sabbir Ahmed Nirjon 
+                                Saif Sabbir
                             </h1>
-                            <p className="text-sm text-muted-foreground">Adjunct Faculty of CSE</p>
-                            <p className="text-sm text-muted-foreground">International Islamic University, Chittagong</p>
+                            <p className="text-sm text-muted-foreground">Adjunct Lecturer</p>
+                            <p className="text-sm text-muted-foreground">Department of Computer Science & Engineering</p>
                             <p className="text-sm text-muted-foreground">
-                                Computer Science & Engineering Dept.
+                                International Islamic University, Chittagong
                             </p>
-                            <p className="text-sm text-muted-foreground">Chattogram, 4318</p>
-                            <p className="text-sm text-muted-foreground">Email: <span className="text-blue-600 underline">saifsabbir2k18@gmail.com</span></p>
+                            <p className="text-sm text-muted-foreground">Kumira, Chattogram-4318, Bangladesh</p>
+                            <p className="text-sm text-muted-foreground">Email: <span className="text-blue-600 underline">saifsabbir.cuet18@gmail.com</span></p>
                         </div>
 
                         {/* Social Icons */}
                         <div className="flex items-center gap-3 mt-6">
-                            <a href="https://www.facebook.com/saifsabbirnirjon" className="social-icon" aria-label="Facebook">
-                                <FaFacebookSquare className="hover:text-gray-600" size={40} />
+                            <a href="https://www.researchgate.net/profile/Saif-Sabbir" className="social-icon" aria-label="Facebook">
+                                <FaResearchgate className="hover:text-gray-600" size={40} />
                             </a>
                             <a href="https://github.com/Sabbir-Saif" className="social-icon" aria-label="GitHub">
                                 <FaGithubSquare className="hover:text-gray-600" size={40} />
                             </a>
                             <a href="saifsabbir2k18@gmail.com" className="social-icon" aria-label="Email">
-                                <FaEnvelope className="hover:text-gray-600"  size={40} />
+                                <FaEnvelope className="hover:text-gray-600" size={40} />
+                            </a>
+                            <a href="https://www.linkedin.com/in/saif-sabbir-23a6531b8" className="social-icon" aria-label="GitHub">
+                                <FaLinkedin className="hover:text-gray-600" size={40} />
                             </a>
                         </div>
                     </div>
@@ -55,7 +58,7 @@ const HeroSection = () => {
                     {/* Bio Content */}
                     <div className="lg:col-span-2 space-y-6 animate-fade-in md:max-w-11/12 text-center md:text-start">
                         <p className="text-lg leading-8 text-foreground/90 ">
-                            I am an adjunct faculty in the{" "}
+                            I am an adjunct faculty member in the{" "}
                             <a href="#" className="text-link">
                                 Computer Science and Engineering Department
                             </a>{" "}
@@ -63,29 +66,17 @@ const HeroSection = () => {
                             <a href="#" className="text-link">
                                 International Islamic University, Chittagong
                             </a>
-                            . I graduated from Chittagong University of Engineering & Technology with B.Sc degree in
-                            Electrical and Electronics Engineering, received my Ph.D. in
-                            Computer Science from Brown University, advised by Professor Sorin
-                            Istrail, and completed my postdoctoral work at Princeton University with
-                            Professor Barbara Engelhardt. My research aims to develop probabilistic
-                            machine learning models, combinatorial algorithms, and scalable
-                            inference methods to better understand high-dimensional data, particularly
-                            genomics and genetics data applied to complex disease.
+                            . I graduated from Chittagong University of Engineering & Technology in June 2024 with B.Sc degree in
+                            Electrical and Electronic Engineering. My research interest follows deep learning, image processing, computer vision and natural language processing. Currently, I am working with real-time video analysis, image classification and segmentation methods, deep learning and transfer learning models etc. Additionally, I also dream for transitioning the current healthcare system with AI-driven smooth
+                            disease diagnosis, drug discovery and surveillance of patients. I believe that having the opportunity to work with leading researchers from various disciplines in ECE and CS may help me shaping this goal.
                         </p>
 
                         <p className="text-lg leading-8 text-text-highlight text-[#0000FF]">
-                            I am looking for <span className="font-bold">motivated</span> PhD students
-                            interested in <span className="font-semibold text-[#6B7770]">foundational</span> research
-                            at the intersection of statistics, probabilistic machine learning, and
-                            algorithms and applying this work to application areas including genomics,
-                            transcriptomics, population genetics, and immunology.
+                            I am looking for a fully funded PhD position as a <span className="font-bold"> Graduate Research Assistant/Teaching Assistant</span> in USA.
+                            I am interested to join <span className="font-bold">the PhD program in ECE/CS department</span> of a reputed university with possible funding facilities.
                         </p>
 
-                        <div className="pt-4">
-                            <a href="#" className="text-link text-lg underline text-[#006376]">
-                                Recent press article about the lab.
-                            </a>
-                        </div>
+                        
                     </div>
                 </div>
             </div>
